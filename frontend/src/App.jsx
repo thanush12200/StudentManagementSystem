ec5f1e2f41d57d0601b16e8e05e418083b5bc91b
thanush12200 <thanush.hpt@gmail.com>
2025-09-23 18:36:04 +0530

    Added the student table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [students] = useState([
+    { id: 1, name: 'Thanush', age: 21 },
+    { id: 2, name: 'Pragna', age: 20 },
+    { id: 3, name: 'Swaroopa', age: 17 },
+    { id: 4, name: 'HariShankar', age: 22 }
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h1>Student Table</h1>
+      <div className='table'> 
+      <table border="1" cellPadding='8' >
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student) => (
+            <tr key={student.id}>
+              <td>{student.id}</td>
+              <td>{student.name}</td>
+              <td>{student.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
