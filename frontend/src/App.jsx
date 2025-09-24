ef4a2dfb830e5076eeca7655c965a3c57b8e8dde
thanush12200 <thanush.hpt@gmail.com>
2025-09-24 18:56:16 +0530

    added css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [students] = useState([
-    { id: 1, name: 'Thanush', age: 21 },
-    { id: 2, name: 'Pragna', age: 20 },
-    { id: 3, name: 'Swaroopa', age: 17 },
-    { id: 4, name: 'HariShankar', age: 22 }
-  ]);
+  const students = [
+    { id: 1, name: "Thanush", age: 21 },
+    { id: 2, name: "Pragna", age: 20 },
+    { id: 3, name: "Swaroopa", age: 19 },
+    { id: 4, name: "Harishankar", age: 22 },
+  ];
 
   return (
-    <div className="App">
-      <h1>Student Table</h1>
-      <div className='table'> 
-      <table border="1" cellPadding='8' >
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Age</th>
-          </tr>
-        </thead>
-        <tbody>
-          {students.map((student) => (
-            <tr key={student.id}>
-              <td>{student.id}</td>
-              <td>{student.name}</td>
-              <td>{student.age}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="app-container">
+      <div className="card">
+        <div className="header">
+          <h1>Student List</h1>
+          <button className="create-student">Create Student</button>
+        </div>
+
+        <div className="table">
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {students.map((student) => (
+                <tr key={student.id}>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.age}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
